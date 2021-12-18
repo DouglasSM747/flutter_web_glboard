8af19a7cfd142f89fba5ef358d52a15275216c05
Douglas <dsm.lic17@uea.edu.br>
2021-12-17 22:07:38 -0400

    alter router

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,6 +1,6 @@
 import 'package:glboard_web/src/features/register_user/models/register_model.dart';
 
-String urlbackend = "http://192.168.1.9:5000";
+String urlbackend = "https://api-backend-gla.herokuapp.com";
 
 var globalUserModel = UserModel("", "", "");
 
